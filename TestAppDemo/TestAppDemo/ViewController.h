0d08e587b0cc2b861f7fdc0926f94488bcfa313f
Harrison MA <harrison@harriso.local>
2018-04-24 14:20:59 +0800

    初始化项目

---
+++ TestAppDemo/TestAppDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAppDemo
+//
+//  Created by Harrison MA on 2018/4/24.
+//  Copyright © 2018年 com.shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestAppDemo/TestAppDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestAppDemo
+//
+//  Created by Harrison MA on 2018/4/24.
+//  Copyright © 2018年 com.shine. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor greenColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
